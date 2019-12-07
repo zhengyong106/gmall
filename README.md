22bd99dc0682790237eacaf34bec99c4eb2c144a
zhengyong <1192133596@qq.com>
2019-12-08 05:05:46 +0800

    添加item模块

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# gmall
-> 尚硅谷谷粒商城
+# 尚硅谷谷粒商城后端
+## gmall-parent
 ## gmall-api
 ## gmall-common-util
-## gmall-manage-service
-## gmall-manage-web
-## gmall-parent
 ## gmall-service-util
-## gmall-user-service
-## gmall-user-web
 ## gmall-web-util
+## gmall-item-web 
+> 端口号 8021
+## gmall-manage-service
+> 端口号 8011
+## gmall-manage-web
+ > 端口号 8012
+## gmall-user-service
+ > 端口号 8001
+## gmall-user-web
+ > 端口号 8002
+# 尚硅谷谷粒商城前端
+## gmall-admin
+ > 端口号 8888
--- gmall-item-web/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
@@ -1,10 +1,18 @@
 package com.atguigu.gmall.item.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @CrossOrigin
-@RestController
+@Controller
 public class ItemController {
-
+    @RequestMapping("item")
+    public String index(ModelMap modelMap) {
+        modelMap.put("123", "231");
+        modelMap.put("222", "3332221");
+        System.out.println("119213");
+        return "item";
+    }
 }
